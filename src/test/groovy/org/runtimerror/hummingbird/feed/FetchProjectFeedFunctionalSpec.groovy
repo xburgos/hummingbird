cd347ddf94d3e5ff94bff18f6bfd240343120d26
Xabier Burgos <xabier.burgos@gmail.com>
2013-08-24 17:58:09 +0100

    Added first dummy test as check that the gradle/spock/java integration works.

---
+++ src/test/groovy/org/runtimerror/hummingbird/feed/FetchProjectFeedFunctionalSpec.groovy
@@ -0,0 +1,13 @@
+package org.runtimerror.hummingbird.feed
+
+import spock.lang.Specification
+
+class FetchProjectFeedFunctionalSpec extends Specification {
+	
+	def 'fetch'() {
+		
+		expect: 
+			1 == 1
+	}
+
+}
